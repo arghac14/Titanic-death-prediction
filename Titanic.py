7dfa79798a322b1130c5ff83335402515f456f6c
imargha <54704761+imargha@users.noreply.github.com>
2019-10-04 15:46:40 +0530

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+#Data reading
 df=pd.read_csv("train.csv")
 
+#Data preprocessing
 mean=df['Age'].mean()
 
 df['Age'].fillna(mean,inplace=True)
@@ -22,9 +25,12 @@ df.drop(['PassengerId','Sex','Age','Pclass','Name','Ticket','Cabin','SibSp','Par
 x=df.drop('Survived',axis=1)
 y=df['Survived']
 
+#Model
 x_train,x_test,y_train,y_test=train_test_split(x, y, test_size=0.2, random_state=6)
 reg=LogisticRegression()
 reg.fit(x_train,y_train)
 pred=reg.predict(x_test)
 print(pred)
+
+#Accuracy
 print(accuracy_score(y_test,pred))
